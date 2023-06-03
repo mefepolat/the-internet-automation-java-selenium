5690e89e9b0c96c6a33bc31ceec3051563b41cb0
Efe Polat <mefepolat@hotmail.com>
2023-06-03 16:49:55 -0400

    dropdown tests

---
+++ src/test/java/dropdown/DropdownTests.java
@@ -0,0 +1,22 @@
+package dropdown;
+
+import base.BaseTests;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class DropdownTests extends BaseTests {
+
+    @Test
+    public void testSelectOption(){
+        var dropDownPage = homePage.clickDropDown();
+        String option = "Option 1";
+        dropDownPage.selectFromDropdown(option);
+        var selectedOptions = dropDownPage.getSelectedOptions();
+        assertEquals(selectedOptions.size(), 1, "Incorrect number of selections");
+
+        assertTrue(selectedOptions.contains(option), "Option not selected");
+
+    }
+}
