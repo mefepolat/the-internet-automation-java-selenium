f5a2d46deac82c183ab25a0e9df19cdac89e16de
Efe Polat <mefepolat@hotmail.com>
2023-06-05 22:57:57 -0400

    More Alert Tests

--- src/main/java/pages/AlertsPage.java
+++ src/main/java/pages/AlertsPage.java
@@ -9,12 +9,32 @@ public class AlertsPage {
 
     private By triggerAlertButton = By.xpath(".//button[text()='Click for JS Alert']");
 
+    private By triggerConfirmButton = By.xpath(".//button[text()='Click for JS Confirm']");
+
+    private By triggerPromptButton = By.xpath(".//button[text()='Click for JS Prompt']");
+
     private By resultMessage = By.id("result");
     public AlertsPage(WebDriver driver){
         this.driver = driver;
     }
     public void triggerAlert(){
         driver.findElement(triggerAlertButton).click();
+    }
+
+    public void triggerPromptButton(){
+        driver.findElement(triggerPromptButton).click();
+    }
+
+    public void triggerConfirmButton(){
+        driver.findElement(triggerConfirmButton).click();
+    }
+
+    public void setPromptMessage(String text){
+        driver.switchTo().alert().sendKeys(text);
+    }
+
+    public void cancelAlert(){
+        driver.switchTo().alert().dismiss();
     }
 
     public void acceptAlert(){
--- src/test/java/alert/AlertTests.java
+++ src/test/java/alert/AlertTests.java
@@ -14,4 +14,30 @@ public class AlertTests extends BaseTests {
         alertPage.acceptAlert();
         assertEquals(alertPage.getResultMessage(), "You successfully clicked an alert", "Result message is incorrect");
     }
+
+    @Test
+    public void testConfirm(){
+        var alertPage = homePage.clickAlertsLink();
+        alertPage.triggerConfirmButton();
+        alertPage.cancelAlert();
+        assertEquals(alertPage.getResultMessage(), "You clicked: Cancel", "Result message is incorrect");
+    }
+
+    @Test
+    public void testConfirmAccept(){
+        var alertPage = homePage.clickAlertsLink();
+        alertPage.triggerConfirmButton();
+        alertPage.acceptAlert();
+        assertEquals(alertPage.getResultMessage(), "You clicked: Ok", "Result message is incorrect");
+    }
+
+    @Test
+    public void testPrompt(){
+        var alertPage = homePage.clickAlertsLink();
+        String message = "a message";
+        alertPage.triggerPromptButton();
+        alertPage.setPromptMessage(message);
+        alertPage.acceptAlert();
+        assertEquals(alertPage.getResultMessage(), "You entered: a message", "Result message is incorrect!");
+    }
 }
